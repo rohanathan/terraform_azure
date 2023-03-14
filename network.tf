5f7d8c64f21099587e936053681018303388d0f2
Rohanathan S <41891930+rohanathan@users.noreply.github.com>
2023-03-14 14:58:35 +0000

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,14 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tfvnet"
+  location            = "Central India"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "tfsubnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.10.0/24"]
+}
+
